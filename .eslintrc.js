e92d6c3655496295b9a1266c2d6c1c0be580461f
BertonWalker <98973545+BertonWalker@users.noreply.github.com>
2023-04-10 18:16:12 +0800

    Update package.json tsconfig.json .eslintrc.js
    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,98 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:compat/recommended',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:markdown/recommended',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    polyfills: ['Promise', 'URL'],
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'markdown'],
+  // https://github.com/typescript-eslint/typescript-eslint/issues/46#issuecomment-470486034
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': 2,
+        '@typescript-eslint/consistent-type-imports': [2, { disallowTypeAnnotations: false }],
+      },
+    },
+    {
+      // In v2, explicitly apply eslint-plugin-markdown's `markdown`
+      // processor on any Markdown files you want to lint.
+      files: ['*.md'],
+      processor: 'markdown/markdown',
+    },
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/state-in-constructor': 0,
+    'react/destructuring-assignment': 0, // TODO: remove later
+    'react/require-default-props': 0,
+    'react/sort-comp': 0,
+    'react/display-name': 0,
+    'react/static-property-placement': 0,
+    'react/no-find-dom-node': 0,
+    'react/no-unused-prop-types': 0,
+    'react/default-props-match-prop-types': 0,
+    'react-hooks/rules-of-hooks': 2, // Checks rules of Hooks
+    'react/function-component-definition': 0,
+    'react/no-unused-class-component-methods': 0,
+    'import/extensions': 0,
+    'import/no-cycle': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          'site/**',
+          'tests/**',
+          'scripts/**',
+          '**/*.test.js',
+          '**/__tests__/*',
+          '*.config.js',
+          '**/*.md',
+        ],
+      },
+    ],
+    'comma-dangle': ['error', 'always-multiline'],
+    'consistent-return': 0, // TODO: remove later
+    'no-param-reassign': 0, // TODO: remove later
+    'no-underscore-dangle': 0,
+    // for (let i = 0; i < len; i++)
+    'no-plusplus': 0,
+    // https://eslint.org/docs/rules/no-continue
+    // labeledLoop is conflicted with `eslint . --fix`
+    'no-continue': 0,
+    // ban this for Number.isNaN needs polyfill
+    'no-restricted-globals': 0,
+    'max-classes-per-file': 0,
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2540#issuecomment-692866111
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 2,
+    'no-shadow': 0,
+    '@typescript-eslint/no-shadow': [2, { ignoreTypeValueShadow: true }],
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2528#issuecomment-689369395
+    'no-undef': 0,
+  },
+  globals: {
+    gtag: true,
+  },
+};
