e6dc392a8513839c7710f3b87c1a7d680b30b8ca
lanlonggang <lanlonggang@tal.com>
2023-04-10 16:05:11 +0800

    Build outputs with diffrent format.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,12 +4,24 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'src/index.ts',
-  output: [{
-    file: 'dist/index.js',
-    format: 'umd',
-    name: 'default',
-    sourcemap: true,
-  }],
+  output: [
+    {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'WHIP',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    }
+  ],
   plugins: [
     json(),
     commonjs(),
