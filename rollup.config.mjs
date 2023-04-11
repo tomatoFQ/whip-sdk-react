05deedacf0b7d5ca4e4a5c1fdde0b9e4559f69a8
lanlonggang <lanlonggang@tal.com>
2023-04-11 10:24:12 +0800

    Delete UMD output config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,12 +10,6 @@ export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.umd.js',
-      format: 'umd',
-      name: 'WHIP',
-      sourcemap: true,
-    },
-    {
       file: 'dist/index.esm.js',
       format: 'esm',
       sourcemap: true,
