a48cc3e5299867077a203233ed23478aa02169b1
lanlonggang <lanlonggang@tal.com>
2023-04-19 18:14:06 +0800

    Update dist

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,9 +1,11 @@
-export declare function usePublish(): {
+import Publisher from './publish';
+export interface PublishHook {
     audioMuted: boolean;
     videoMuted: boolean;
     peerconnection: RTCPeerConnection;
-    init: any;
-    publish: any;
-    mute: any;
-    delete: any;
-};
+    init: typeof Publisher.prototype.init;
+    publish: typeof Publisher.prototype.publish;
+    mute: typeof Publisher.prototype.mute;
+    delete: typeof Publisher.prototype.unpublish;
+}
+export declare function usePublish(): PublishHook;
